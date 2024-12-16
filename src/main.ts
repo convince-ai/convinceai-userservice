aa2cc5b73570cb2f6870a44ef24b96537d637da1
Leonardo Candido <leonardo.leotc@gmail.com>
2024-12-16 16:10:52 -0300

    fix: running port

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT ?? 8080;
   const app = await NestFactory.create(AppModule);
 
   // Habilitar validação de DTOs
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
